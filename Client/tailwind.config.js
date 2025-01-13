16b2b60c4c3001cbedc650ed3072db5465200a05
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2025-01-13 18:25:29 +0200

    init

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
