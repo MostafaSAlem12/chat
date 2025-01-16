fd48b5a2ac2159695301c43955bb37a72fc2ca9b
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2025-01-16 09:41:34 +0200

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -4,9 +4,21 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 
-dotenv.config();
+dotenv.config({ path: "./config.env" });
+
 
 const app = express();
 const port = process.env.PORT || 3000 ;
+const DB = process.env.DATABASE_URL;
 
 
+const server = app.listen(port ,()=>{
+    console.log(`server is running on port ${port}`)
+})
+
+
+
+mongoose
+.connect(DB)
+.then(() =>console.log("Database connection successful")
+.catch(err=>console.log(err.message)));
