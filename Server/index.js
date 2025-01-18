3795356be09fb7e72cbd71258565c5d787563cd7
Mostafa Salem Ali <86600378+MostafaSAlem12@users.noreply.github.com>
2025-01-18 13:26:55 +0200

    add userModel

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,15 @@ const app = express();
 const port = process.env.PORT || 3000 ;
 const DB = process.env.DATABASE_URL;
 
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    credentials:true,
+    
+}))
 
+app.use(cookieParser());
+app.use(express.json())
 const server = app.listen(port ,()=>{
     console.log(`server is running on port ${port}`)
 })
